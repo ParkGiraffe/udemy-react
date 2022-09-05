26c7188595f88127fccaa20bc7043dbd461d2b83
ParkGiraffe <op5321@soongsil.ac.kr>
2022-09-05 16:18:31 +0900

    Study: 3-30 study finish

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
+    // jsx - react팀에서 개발한 구문으로 js파일에서 html 문구를 사용할 수 있게 한다. 백그라운드에서 자동 변환 과정이 일어난다.
     <div>
       <h2>Let's get started!</h2>
+      <h3>[lecture 30]</h3>
     </div>
   );
 }
