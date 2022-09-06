20024ebcb5cf34b1b034999b5da35365fbadd33a
ParkGiraffe <op5321@soongsil.ac.kr>
2022-09-06 17:17:22 +0900

    Study: 3-33 study finish

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,9 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
-    // jsx - react팀에서 개발한 구문으로 js파일에서 html 문구를 사용할 수 있게 한다. 백그라운드에서 자동 변환 과정이 일어난다.
+    // jsx[javascript xml] - react팀에서 개발한 구문으로 js파일에서 html 문구를 사용할 수 있게 한다. 백그라운드에서 자동 변환 과정이 일어난다.
     <div>
-      <h2>Let's get started!</h2>
-      <h3>[lecture 30]</h3>
+      <h2>Let's get started!</h2> // 소문자로 시작 - html
+      <ExpenseItem></ExpenseItem> // 대문자로 시작 - 컴포넌트
     </div>
   );
 }
