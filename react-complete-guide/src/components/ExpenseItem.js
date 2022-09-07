be8d9be2a5c4cdd188f05a74f0ba2aa38487ffa6
ParkGiraffe <op5321@soongsil.ac.kr>
2022-09-07 16:28:35 +0900

    Study: 3-34 study finish

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>
+  return (
+    // 컴포넌트에서 jsx를 return할 때는 반드시 한 개의 루트 요소를 가진다. 그래서 <div></div>로 전체를 하나로 묶었다.
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
