c8958b1c4f72090d3bf05c371c50e2be9f4af5a6
ParkGiraffe <op5321@soongsil.ac.kr>
2022-09-09 23:05:58 +0900

    Study: 3-37 doing

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,21 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amoount: 294.67,
+      date: new Date(2022, 2, 20),
+    },
+  ];
   return (
     // jsx[javascript xml] - react팀에서 개발한 구문으로 js파일에서 html 문구를 사용할 수 있게 한다. 백그라운드에서 자동 변환 과정이 일어난다.
     <div>
-      <h2>Let's get started!</h2> // 소문자로 시작 - html
-      <ExpenseItem></ExpenseItem> // 대문자로 시작 - 컴포넌트
+      {/* 소문자로 시작 - html */}
+      <h2>Let's get started!</h2>
+      {/* 대문자로 시작 - 컴포넌트 */}
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -8,10 +8,12 @@ function ExpenseItem() {
 
   return (
     // 컴포넌트에서 jsx를 return할 때는 반드시 한 개의 루트 요소를 가진다. 그래서 <div></div>로 전체를 하나로 묶었다.
-    <div className = 'expense-item'> //JSX에서는 html의 class 명을 지을 때 class가 아닌 className을 사용해서 class명을 짓는다. (왜냐하면 class가 js에서 예약어기 때문이다.)
-      <div className='expense-item__description'>{expenseDate}</div>
+    <div className = 'expense-item'> 
+    {/* jsx에서는 html의 class 명을 지을 때 class가 아닌 className을 사용해서 class명을 짓는다. (왜냐하면 class가 js에서 예약어기 때문이다.) */}
+      <div className='expense-item__description'>{expenseDate.toISOString()}</div>
       <div className='expense-item__price'>
-        <h2>{expenseTitle}</h2> jsx 안에 중괄호를 사용하면 일반 자바스크립트 구문을 사용할 수 있다.
+        <h2>{expenseTitle}</h2> 
+        {/* jsx 안에 중괄호를 사용하면 일반 자바스크립트 구문을 사용할 수 있다. */}
         <div>${expenseAmount}</div>
       </div>
     </div>
