43f504107eab431babef1f6e555db0ac79a5d5e4
ParkGiraffe <op5321@soongsil.ac.kr>
2022-09-23 15:09:10 +0900

    Study: 4-55 done

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const titleChangeHandler = () => {
+    console.log('Title changed!');
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
+          {/* 사용자 입력을 받을 때 onChange props를 이용하면 모든 종류의 입력 타입에 대응할 수 있다. 인자로는 함수를 받는다. */}
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
