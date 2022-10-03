24c54bc611777d245bf6be899bd885c1bac45f57
ParkGiraffe <op5321@soongsil.ac.kr>
2022-10-03 15:59:15 +0900

    Study: 5-73 done

--- react-complete-guide/src/components/Expense/Expenses.js
+++ react-complete-guide/src/components/Expense/Expenses.js
@@ -15,6 +15,22 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expenseContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    {
+      /*JS에 내장된 Array.prototype.map()을 이용해서 Array로 이루어진 Props를 한 번에 컴포넌트에 렌더링할 수 있다. map() array를 리턴하는데, 리액트는 jsx(컴포넌트)로 이루어진 array를 자동으로 나란히 렌더링해준다. */
+    }
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -22,16 +38,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={changeFilterHandler}
         />
-
-        {/*JS에 내장된 Array.prototype.map()을 이용해서 Array로 이루어진 Props를 한 번에 컴포넌트에 렌더링할 수 있다. map() array를 리턴하는데, 리액트는 jsx(컴포넌트)로 이루어진 array를 자동으로 나란히 렌더링해준다. */}
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expenseContent}
       </Card>
     </div>
   );
