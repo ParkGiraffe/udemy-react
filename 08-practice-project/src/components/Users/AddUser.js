88b1d2429e33fef158d7640a0a9bc1b324492456
ParkGiraffe <op5321@soongsil.ac.kr>
2022-10-11 21:10:39 +0900

    Study: 8-101 done

--- 08-practice-project/src/components/Users/AddUser.js
+++ 08-practice-project/src/components/Users/AddUser.js
@@ -1,26 +1,37 @@
-import React from 'react'
-import Button from '../UI/Button';
-import Card from '../UI/Card';
-import styles from './AddUser.module.css';
-
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
-  }
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
-    // Card는 커스텀 컴포넌트이고 내장 Html 컴포넌트가 아니다. 
+    // Card는 커스텀 컴포넌트이고 내장 Html 컴포넌트가 아니다.
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         {/* html의 for attribute를 jsx에서는 htmlFor이라고 부른다. for은 js의 예약어라서 그렇다. */}
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
-  )
-}
+  );
+};
 
-export default AddUser
+export default AddUser;
