84265db5fdc4ff652c089186dc66be01359efe86
ParkGiraffe <op5321@soongsil.ac.kr>
2022-10-11 21:57:57 +0900

    Study: 8-105 done

---
+++ 08-practice-project/src/components/UI/ErrorModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from './Button';
+import Card from './Card';
+import styles from './ErrorModal.module.css';
+
+const ErrorModal = (props) => {
+  return(
+    <div>
+      <div className={styles.backdrop}/> {/* div를 하나 추가해준다음, Backdrop 스타일을 추가해주면 뒷배경 상호작용을 막을 수 있다. */}
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  )
+}
--- 08-practice-project/src/components/Users/UsersList.js
+++ 08-practice-project/src/components/Users/UsersList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../UI/Card";
+import styles from "./UsersList.module.css";
 
 const UsersList = (props) => {
   return (
