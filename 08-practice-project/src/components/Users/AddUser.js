79e2e852da49a39194365064cb9664cc7efaf210
ParkGiraffe <op5321@soongsil.ac.kr>
2022-10-11 21:15:43 +0900

    Study: 8-102 done

--- 08-practice-project/src/components/Users/AddUser.js
+++ 08-practice-project/src/components/Users/AddUser.js
@@ -9,6 +9,16 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    
+    // 유효성 검사
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (event) => {
@@ -25,9 +35,19 @@ const AddUser = (props) => {
       <form onSubmit={addUserHandler}>
         {/* html의 for attribute를 jsx에서는 htmlFor이라고 부른다. for은 js의 예약어라서 그렇다. */}
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          value={enteredAge}
+        />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}/>
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredUsername}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
