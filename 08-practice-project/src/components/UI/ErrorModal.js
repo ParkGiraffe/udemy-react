c49d04320e3b5e79a49584121b238ba333948fb1
ParkGiraffe <op5321@soongsil.ac.kr>
2022-10-12 20:08:00 +0900

    Study: 8-106 done

--- 08-practice-project/src/components/UI/ErrorModal.js
+++ 08-practice-project/src/components/UI/ErrorModal.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import Button from './Button';
-import Card from './Card';
-import styles from './ErrorModal.module.css';
+import React from "react";
+import Button from "./Button";
+import Card from "./Card";
+import styles from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
-  return(
+  return (
     <div>
-      <div className={styles.backdrop}/> {/* div를 하나 추가해준다음, Backdrop 스타일을 추가해주면 뒷배경 상호작용을 막을 수 있다. */}
+      <div className={styles.backdrop} onClick={props.onConfirm} />{" "}
+      {/* div를 하나 추가해준다음, Backdrop 스타일을 추가해주면 뒷배경 상호작용을 막을 수 있다. */}
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
@@ -15,9 +16,12 @@ const ErrorModal = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={styles.actions}>
-          <Button>Okay</Button>
+          <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
     </div>
-  )
-}
+  );
+};
+
+
+export default ErrorModal;
--- 08-practice-project/src/components/Users/AddUser.js
+++ 08-practice-project/src/components/Users/AddUser.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    
+
     // 유효성 검사
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -30,28 +40,41 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    // Card는 커스텀 컴포넌트이고 내장 Html 컴포넌트가 아니다.
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        {/* html의 for attribute를 jsx에서는 htmlFor이라고 부른다. for은 js의 예약어라서 그렇다. */}
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={usernameChangeHandler}
-          value={enteredAge}
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-        <label htmlFor="age">Age (years)</label>
-        <input
-          id="age"
-          type="number"
-          onChange={ageChangeHandler}
-          value={enteredUsername}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+      {/* // Card는 커스텀 컴포넌트이고 내장 Html 컴포넌트가 아니다. */}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          {/* html의 for attribute를 jsx에서는 htmlFor이라고 부른다. for은 js의 예약어라서 그렇다. */}
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={usernameChangeHandler}
+            value={enteredAge}
+          />
+          <label htmlFor="age">Age (years)</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={enteredUsername}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
