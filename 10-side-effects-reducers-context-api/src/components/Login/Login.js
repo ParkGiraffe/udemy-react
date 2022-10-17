f9fcb349c2f0268f163eea41afcd400cac37a678
ParkGiraffe <op5321@soongsil.ac.kr>
2022-10-17 16:27:59 +0900

    Study: 10-122 done

--- 10-side-effects-reducers-context-api/src/components/Login/Login.js
+++ 10-side-effects-reducers-context-api/src/components/Login/Login.js
@@ -13,13 +13,16 @@ const Login = (props) => {
 
 
   useEffect(() => {
+    // setTimeout을 식별자(identifier)변수에 저장한 후, 브라우저 내장함수인 cleartimeout을 사용하여 setTimeout을 지울 수 있다. 0.5초 안에 value가 입력이 되면 setTimeout이 지워지기 때문에, useEffect의 의존성 변경에 따라 다시 처음부터 setTimeout이 실행되어 0.5초를 센다.
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
       setFormIsValid(
         enteredEmail.includes('@') && enteredPassword.trim().length > 6
       );
     }, 500);
+    // 타이핑 시 작동 순서 : cleanup 콘솔이 뜬 이후에 'checkig form validity'가 뜬다. 
 
+    // 사용자가 다 입력한 후 유효성 검사 : 디바운싱이라고 표현. 사용자 입력을 디바운스(그룹화)
     return () => {
       console.log('CLEANUP');
       clearTimeout(identifier);
