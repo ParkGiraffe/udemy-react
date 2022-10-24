13403f94825f60960b25e4310150f384325f917f
ParkGiraffe <op5321@soongsil.ac.kr>
2022-10-24 15:27:54 +0900

    Study: 10-136 done

--- 10-side-effects-reducers-context-api/src/components/Login/Login.js
+++ 10-side-effects-reducers-context-api/src/components/Login/Login.js
@@ -97,34 +97,22 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={email.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input 
+          id = 'email'
+          label = 'E-mail'
+          type = 'email'
+          isValid = {emailIsValid}
+          onChange = {emailChangeHandler}
+          onBlur = {validateEmailHandler}
+        />
+        <Input 
+          id = 'password'
+          label = 'Password'
+          type = 'password'
+          isValid = {passwordIsValid}
+          onChange = {passwordChangeHandler}
+          onBlur = {validatePasswordHandler}
+        />
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
