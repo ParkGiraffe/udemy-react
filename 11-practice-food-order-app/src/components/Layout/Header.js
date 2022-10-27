2b89deca3e1a6e46a465cc12996735bb1305c8b0
ParkGiraffe <op5321@soongsil.ac.kr>
2022-10-27 16:17:39 +0900

    Study: 11-143 done

--- 11-practice-food-order-app/src/components/Layout/Header.js
+++ 11-practice-food-order-app/src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import mealsImage from "../../assets/meals.jpg";
-import classes from "./Header.moludle.css";
+import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
