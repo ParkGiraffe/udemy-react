5ecd06fa4480296eb22fbee0bca9701ee212a0b3
ParkGiraffe <op5321@soongsil.ac.kr>
2022-11-14 15:05:12 +0900

    Study: 12-162 done

--- 12-a-look-behind-the-scenes/src/App.js
+++ 12-a-look-behind-the-scenes/src/App.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import Button from './components/UI/Button/Button';
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
-  const toggleParagraphHandler = () =>{
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  }
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
