57d1e7671d91f6caafa6b643f1c596a2906980a7
ParkGiraffe <op5321@soongsil.ac.kr>
2022-12-02 16:17:12 +0900

    Study: 15-204 done

--- 15.2-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
+++ 15.2-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
@@ -1,43 +1,32 @@
-import { useState } from 'react';
+import { useState } from "react";
+import useHttp from "../../hooks/use-http";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskReqeust } = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6e4f6-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  const createdTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    props.onAddTask(createdTask);
   };
 
+  const enterTaskHandler = async (taskText) => {
+    sendTaskReqeust(
+      {
+        url: "https://react-http-6e4f6-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createdTask.bind(null, taskText)
+    );
+  };
   return (
     <Section>
       <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
