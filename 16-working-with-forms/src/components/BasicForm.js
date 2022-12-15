a6701ce152d7054b69c8b23e0071dd5a125eb2e3
ParkGiraffe <op5321@soongsil.ac.kr>
2022-12-15 16:10:42 +0900

    Study: 16-222 done

--- 16-working-with-forms/src/components/BasicForm.js
+++ 16-working-with-forms/src/components/BasicForm.js
@@ -1,22 +1,109 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  const isNotEmpty = (value) => value.trim() !== "";
+  const isEmail = (value) => value.includes("@");
+
+  const {
+    value: fName,
+    isValid: fNameIsValid,
+    hasError: fNameHasErr,
+    valueChangeHandler: fNameChangeHandler,
+    inputBlurHandler: fNameBlurHandler,
+    reset: fNameReset,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: lName,
+    isValid: lNameIsValid,
+    hasError: lNameHasErr,
+    valueChangeHandler: lNameChangeHandler,
+    inputBlurHandler: lNameBlurHandler,
+    reset: lNameReset,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailHasErr,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: emailReset,
+  } = useInput(isEmail);
+
+  let formIsValid;
+
+  if (lNameIsValid && fNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!lNameIsValid || !fNameIsValid || !emailIsValid) return;
+
+    lNameReset();
+    fNameReset();
+    emailReset();
+  };
+
+  const lNameInputStyles = lNameHasErr
+    ? "form-control invalid"
+    : "form-control";
+
+  const fNameInputStyles = fNameHasErr
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputStyles = emailHasErr
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={fNameInputStyles}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="fName"
+            onChange={fNameChangeHandler}
+            onblur={fNameBlurHandler}
+            value={fName}
+          />
+          {fNameHasErr && (
+            <p className="error-text">Please enter a valid value</p>
+          )}
         </div>
-        <div className="form-control">
+        <div className={lNameInputStyles}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="lName"
+            onChange={lNameChangeHandler}
+            onblur={lNameBlurHandler}
+            value={lName}
+          />
         </div>
+        {lNameHasErr && (
+          <p className="error-text">Please enter a valid value</p>
+        )}
       </div>
-      <div className="form-control">
+      <div className={emailInputStyles}>
         <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <input
+          type="email"
+          id="email"
+          onChange={emailChangeHandler}
+          onblur={emailBlurHandler}
+          value={email}
+        />
+        {emailHasErr && (
+          <p className="error-text">Please enter a valid value</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
