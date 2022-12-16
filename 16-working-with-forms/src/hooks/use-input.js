826c0d4be05ef1fccab50596ff81c61e9c899df2
ParkGiraffe <op5321@soongsil.ac.kr>
2022-12-16 13:58:40 +0900

    Study: 16-223 done

--- 16-working-with-forms/src/hooks/use-input.js
+++ 16-working-with-forms/src/hooks/use-input.js
@@ -1,27 +1,63 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return {
+      value: action.value,
+      state: state.isTouched, // isTouched는 true가 아니라 기존 값을 그대로 사용하게 끔 한다. 처음에 사용자가 입력을 마쳐야만 에러메시지가 보여주도록 설계하기 위해서이다.
+    };
+  }
+  if (action.type === "BLUR") {
+    return {
+      isTouched: true,
+      value: state.value,
+    };
+  }
+  if (action.type === "RESET") {
+    return {
+      isTouched: false,
+      value: "",
+    };
+  }
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  // const [enteredValue, setEnteredValue] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    // setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
   };
 
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    // setIsTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    // setEnteredValue("");
+    // setIsTouched(false);
+
+    dispatch({ type: "RESET" });
   };
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
