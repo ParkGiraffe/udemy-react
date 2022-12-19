6cf1690418deb12d0e7875761a3ea8bba95c890e
ParkGiraffe <op5321@soongsil.ac.kr>
2022-12-19 15:16:17 +0900

    Study: 17-228 done

--- 17-practice-food-order-http-forms/src/components/Meals/AvailableMeals.js
+++ 17-practice-food-order-http-forms/src/components/Meals/AvailableMeals.js
@@ -1,10 +1,11 @@
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isloading, setIsLoading] = useState(true); // 어차피 앱이 실행되자마자 바로 백엔드에서 데이터를 가져오기 때문에, 초기값을 true로 줘버려도 상관을 없을 경우가 있다.
 
   // useEffect의 콜백함수에 바로 async를 적용하는 것은 금지! - 허용되지 않는 작업이다.
   useEffect(() => {
@@ -24,10 +25,18 @@ const AvailableMeals = () => {
           price: responseData[key].price,
         });
       }
+      setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    setMeals(loadedMeals);
+    fetchMeals(); // useEffect 내부에 async/await 함수를 만들고 호출하는 꼼수를 사용하면 된다.
   }, []);
+
+  if (isloading) {
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
