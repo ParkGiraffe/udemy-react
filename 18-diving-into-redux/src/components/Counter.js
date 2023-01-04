64a3d9d357d1b97f79a791b180cac0a728ea27e7
ParkGiraffe <op5321@soongsil.ac.kr>
2023-01-04 14:56:14 +0900

    Study: 18-249 done

--- 18-diving-into-redux/src/components/Counter.js
+++ 18-diving-into-redux/src/components/Counter.js
@@ -6,10 +6,13 @@ const Counter = () => {
 
   const dispatch = useDispatch();
   const incrementHandler = () => {
-    dispatch({ action: "increment" });
+    dispatch({ type: "increment" });
   };
   const decrementHandler = () => {
-    dispatch({ action: "decrement" });
+    dispatch({ type: "decrement" });
+  };
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 10 });
   };
 
   const toggleCounterHandler = () => {};
@@ -20,6 +23,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
