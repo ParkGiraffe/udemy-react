d53a2095b1e61d4c98d9fd609f082af2cc487100
ParkGiraffe <op5321@soongsil.ac.kr>
2023-01-06 22:27:57 +0900

    Study: 18-251 done

--- 18-diving-into-redux/src/store/index.js
+++ 18-diving-into-redux/src/store/index.js
@@ -5,8 +5,12 @@ const initialState = {
   showCounter: true,
 };
 
+
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
+    // state.counter++; <-- Redux로 작업할 때는 절때 원본 state 객체를 수정해서는 안 된다. 버그 발생 및 프로그램 디버깅에 어려움이 발생한다. UI 더 이상 업데이트가 되지 않을 수도 있다. 그러므로 항상 새로운 객체를 반환해주자.
+
     return {
       counter: state.counter + 1,
       showCounter: state.showCounter,
