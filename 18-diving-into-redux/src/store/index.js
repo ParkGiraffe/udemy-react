741d869380baec2028befe2874d5a58eacc4af5c
ParkGiraffe <op5321@soongsil.ac.kr>
2023-01-11 09:20:12 +0900

    Study: 18-256 done

--- 18-diving-into-redux/src/store/index.js
+++ 18-diving-into-redux/src/store/index.js
@@ -1,14 +1,14 @@
 // import { createStore } from "redux";
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+const initialCounterState = {
   counter: 1,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState: initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -25,16 +25,31 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login() {
+      isAuthenticated = true;
+    },
+    logout() {
+      isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
-
-
-
 
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === "increment") {
