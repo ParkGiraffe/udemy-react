422b665e57a35ee7b48cc83b0a5adc51ee0da200
ParkGiraffe <op5321@soongsil.ac.kr>
2023-01-11 11:14:23 +0900

    Study: 18-258 done

--- 18-diving-into-redux/src/store/index.js
+++ 18-diving-into-redux/src/store/index.js
@@ -1,53 +1,11 @@
 // import { createStore } from "redux";
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-const initialCounterState = {
-  counter: 1,
-  showCounter: true,
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter += action.payload;
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import auth from "./auth";
+import counter from "./counter";
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counter, auth: auth },
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
 
