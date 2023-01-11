b94b908488c388966e7f87015d314ad41413000a
ParkGiraffe <op5321@soongsil.ac.kr>
2023-01-11 15:18:54 +0900

    Study: 19-263 done

---
+++ 19-advanced-redux/src/store/cart-slice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((e) => e.id === newItem.id);
+
+      if (!existingItem) {
+        state.items.push({
+          name: newItem.id,
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
+
+    removeItemFromCart(state) {},
+  },
+});
