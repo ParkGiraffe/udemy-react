f5bfa180fe9d6f8fb5a4ef53dbb045914e3f1f7e
ParkGiraffe <op5321@soongsil.ac.kr>
2023-01-16 09:31:11 +0900

    Study: 20-293 done

--- 20.2-practice-react-router/src/pages/QuoteDetail.js
+++ 20.2-practice-react-router/src/pages/QuoteDetail.js
@@ -1,13 +1,21 @@
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "park", text: "Learning React is so hard!" },
+  { id: "q2", author: "giraffe", text: "Learning React is so fun" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((e) => e.id === params.quoteId);
+  if (!quote) <p>No quote found!</p>;
+
   return (
     <>
-      <h1>QuoteDetail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path="/quotes/:quoteId/comments">
         <Comments />
       </Route>
