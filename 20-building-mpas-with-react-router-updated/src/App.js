3cf3fb8f776cf2291210ec0e6be1390b8288010f
ParkGiraffe <op5321@soongsil.ac.kr>
2023-01-17 10:04:47 +0900

    Study: 20-301 done

--- 20-building-mpas-with-react-router-updated/src/App.js
+++ 20-building-mpas-with-react-router-updated/src/App.js
@@ -1,31 +1,49 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import ProductDetail from "./pages/ProductDetail";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
 function App() {
+  // v6
   return (
     <div>
       <MainHeader />
       <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
+        <Routes>
+          <Route path="/" replace element={<Navigate to="/welcome" />} />
+          <Route path="/welcome/*" element={<Welcome />}>
+            <Route path="new-user" element={<p>Welcome, new user!</p>} />
           </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
       </main>
     </div>
   );
+
+  // v5
+  // return (
+  //   <div>
+  //     <MainHeader />
+  //     <main>
+  //       <Switch>
+  //         <Route path="/" exact>
+  //           <Redirect to="/welcome" />
+  //         </Route>
+  //         <Route path="/welcome">
+  //           <Welcome />
+  //         </Route>
+  //         <Route path="/products" exact>
+  //           <Products />
+  //         </Route>
+  //         <Route path="/products/:productId">
+  //           <ProductDetail />
+  //         </Route>
+  //       </Switch>
+  //     </main>
+  //   </div>
+  // );
 }
 
 export default App;
--- 20-building-mpas-with-react-router-updated/src/pages/Welcome.js
+++ 20-building-mpas-with-react-router-updated/src/pages/Welcome.js
@@ -1,12 +1,11 @@
-import { Route } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <section>
       <h1>Welcome Page</h1>
-      <Route path="/welcome/new-user">
-        <p>Welcome, new user!</p>
-      </Route>
+      <Link to="new-user">New User</Link>
+      <Outlet />
     </section>
   );
 };
