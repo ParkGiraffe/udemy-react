141e362d42cabc7aa757dc5af727aa6e3ea34189
ParkGiraffe <op5321@soongsil.ac.kr>
2023-02-10 15:43:30 +0900

    Study: 27-412 done

---
+++ 27.1-typescript/basics.ts
@@ -0,0 +1,34 @@
+/*
+Primitives: number, string, boolean
+More complex types: arrays, objects
+Function types, parameters
+*/
+
+// Primitives
+let age: number;
+age = 12;
+
+let uesrName: string;
+uesrName = "Max";
+
+let isInstructor: boolean;
+isInstructor = true;
+
+// More complex types
+let hobbies: string[];
+hobbies = ["soccer", "guitar", "music"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "park",
+  age: 23,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
