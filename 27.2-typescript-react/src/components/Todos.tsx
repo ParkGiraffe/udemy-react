7ba861a40f93b795306e2d5dc68c985ad0e9fc78
ParkGiraffe <op5321@soongsil.ac.kr>
2023-03-08 15:42:50 +0900

    Study: 27-439 done

--- 27.2-typescript-react/src/components/Todos.tsx
+++ 27.2-typescript-react/src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 // 직접 생성한 class는 단순히 생성자 역할을 할 뿐만 아니라, type 역할도 한다. class 이름을 그대로 타입으로 사용할 수 있다.
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
