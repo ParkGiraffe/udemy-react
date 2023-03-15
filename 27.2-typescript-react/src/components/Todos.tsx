18218806ff6dd18a9ccb84783d376faafa057cd7
ParkGiraffe <op5321@soongsil.ac.kr>
2023-03-15 15:43:55 +0900

    Study: 27-446 done

--- 27.2-typescript-react/src/components/Todos.tsx
+++ 27.2-typescript-react/src/components/Todos.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import Todo from "../models/todo";
+import React, { useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 import TodoItem from "./TodoItem";
-import classes from './Todos.module.css';
+import classes from "./Todos.module.css";
 
 // 직접 생성한 class는 단순히 생성자 역할을 할 뿐만 아니라, type 역할도 한다. class 이름을 그대로 타입으로 사용할 수 있다.
-const Todos: React.FC<{ items: Todo[], onRemoveTodo: (id: string) => void }> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}/>
+      {todosCtx.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
